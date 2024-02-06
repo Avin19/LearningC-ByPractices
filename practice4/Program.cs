4bfdad8394cf2bb569292dca8f44f6e3cf524028
Avin19 <avinashy1993@gmail.com>
2024-02-06 09:21:22 +0530

    Oops

---
+++ practice4/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace practice4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Enter First Name :");
+        // string name = Console.ReadLine();
+        // Person p1 = new Person(name);
+        // Console.WriteLine(p1.Name);
+        // Console.ReadLine();   
+        Person[] people = new Person[3];
+
+    
+        for( int i= 0 ; i < people.Length ; i ++)
+        {
+            Console.WriteLine(" Enter a Person Name ");
+            string name = Console.ReadLine();
+            people[i] = new Person(name);
+        }
+        foreach(Person p in people)
+        {
+            Console.WriteLine( $" Hello! My name {p.Name}");
+        }
+        
+    }
+}
+
+
+public class Person 
+{
+    public string Name { get; set;}
+
+   public Person(string name)
+   {
+    Name = name;
+   }
+}
---
+++ practice4/Question.md
@@ -0,0 +1,4 @@
+### Create a C# program that requests three names of people from the user and stores them in an array of object of type Person . 
+
+
+# To do this, first create a Person Class that has a Name property of type "String" .
